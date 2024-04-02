f1350ab17b8dccfb115f7b15528ef7a1e1279405
Zane41 <korener@rambler.ru>
2024-04-02 14:37:18 +0300

    выполнено задание добавлены 3 функции

--- src/Main.java
+++ src/Main.java
@@ -46,3 +46,36 @@ class Frog {
     }
 
 }
+class testFunctions{
+    public static Frog createFrog() {
+        System.out.println("Введи координаты начальной точки лягухи и длину её языка");
+        Scanner vvod = new Scanner(System.in);
+        return  new Frog(new Point(vvod.nextInt(), vvod.nextInt()), vvod.nextInt());
+    }
+    public  static Point[] askRoute (){
+        System.out.println("Введи количество точек в маршруте");
+        Scanner vvod= new Scanner(System.in);
+        int routeLenght = vvod.nextInt();
+        System.out.println("Введи координаты этих точек");
+        Point [] route = new Point[routeLenght];
+        for (int i = 0; i < routeLenght; i++) {
+
+            route[i]= new Point(vvod.nextInt(), vvod.nextInt());
+        }
+        return route;
+    }
+    public static void fullTest (){
+         Point[] route = askRoute();int pointsQuantity= route.length; Frog frog=createFrog(); Point startFrog=frog.place;
+        int a = 0;
+        for (int i = 0; i < pointsQuantity; i++) {
+            if (Point.calcDist(startFrog, route[i]) > frog.tongueLenght) {
+                a++; if (a==pointsQuantity) System.out.println("Yes");
+            }
+            else {
+                a++;
+                System.out.println("Лягушка съела Тему в точке " + a);
+                break;
+            }
+        }
+    }
+}
